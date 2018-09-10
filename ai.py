99bb38e7593622f1cbd0ab354c0ef7cdf3e87e74
Ross Nkama <rnkama20@gmail.com>
2018-09-10 21:19:10 +0100

    CNN architecture created

--- ai.py
+++ ai.py
@@ -18,8 +18,25 @@ class CNN(nn.Module):
 	# Number of actions come from the ppaquette action space
 	def __init__(self, no_actions):
 		super(CNN, self).__init__()
-		self.convolution1
-		self.convolution2
-		self.convolution3
-		self.full_conn1
-		self.full_conn2
+		# in_channels = 1 because images are black and white so we don't...
+		# need 3 images corresponding RBG channels.
+		# Going to use 32 image kernels so want 32 convolved images...
+		# as output channels
+		self.convolution1 = nn.Conv2d(
+			in_channels=1, 
+			out_channels=32, 
+			kernel_size=5
+		)
+		self.convolution2 = nn.Conv2d(
+			in_channels=32, 
+			out_channels=32, 
+			kernel_size=3
+		)
+		self.convolution3 = nn.Conv2d(
+			in_channels=32, 
+			out_channels=64, 
+			kernel_size=2
+		)
+
+		self.full_conn1 = nn.Linear(in_features=number_neurons , out_features=40)
+		self.full_conn2 = nn.Linear(in_features=40, out_features=no_actions)
