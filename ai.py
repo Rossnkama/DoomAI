ce104b9c49974c546404b8acdd68f0df24a789a6
Ross Nkama <rnkama20@gmail.com>
2018-09-10 20:58:34 +0100

    AI architecture defined

--- ai.py
+++ ai.py
@@ -10,3 +10,16 @@ from gym.wrappers import SkipWrapper
 from ppaquette_gym_doom.wrappers.action_space import ToDiscrete
 
 import experience_replay, image_preprocessing 
+
+
+# The brain of the AI is a convolutional network
+class CNN(nn.Module):
+
+	# Number of actions come from the ppaquette action space
+	def __init__(self, no_actions):
+		super(CNN, self).__init__()
+		self.convolution1
+		self.convolution2
+		self.convolution3
+		self.full_conn1
+		self.full_conn2
